fd48ec8628f43fc656c8c4d6bec27a108224932d
bkramarski <bkramarski@gmail.com>
2018-12-02 18:01:53 +0100

    Solutions day1-2

---
+++ day1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func opOnInt(initial int64, op rune, value int64) int64 {
+	if op == '-' {
+		return initial - value
+	} else if op == '+' {
+		return initial + value
+	} else {
+		return initial
+	}
+}
+
+func main() {
+	stderr := bufio.NewWriter(os.Stderr)
+    defer stderr.Flush()
+    
+    stdin := bufio.NewReader(os.Stdin)
+    
+	var sum, number, lines int64 = 0, 0, 0
+	var op rune = ' '
+
+	read, err := fmt.Fscanf(stdin, "%c%d\n", &op, &number)
+	if err == nil && read >= 2 {
+		sum = opOnInt(sum, op, number)
+        lines++
+	} else {
+		fmt.Fprintf(stderr, "%s got: %d\n", err, read)
+	}
+
+	for err == nil {
+		read, err = fmt.Fscanf(stdin, "%c%d\n", &op, &number)
+		if err == nil && read >= 2 {
+			sum = opOnInt(sum, op, number)
+            lines++
+		} else {
+			fmt.Fprintf(stderr, "%s got: %d\n", err, read)
+		}
+	}
+	fmt.Printf("%d\n", sum)
+    fmt.Fprintf(stderr, "Lines processed: %d\n", lines)
+}
---
+++ day2b.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+    "strings"
+)
+
+func distance(lhs, rhs string) (int, string) {
+    if len(lhs) != len(rhs) {
+        panic("bad input")
+    }
+    different := 0
+    common := strings.Builder{}
+    l, r := strings.NewReader(lhs), strings.NewReader(rhs)
+
+    for ;; {
+        left, _, err_l := l.ReadRune()
+        right, _, err_r := r.ReadRune()
+        
+        if err_l != nil || err_r != nil {
+            break
+        }        
+        
+        if left == right {
+            common.WriteRune(left)
+        } else {
+            different++
+        }
+    }
+    return different, common.String()
+}
+
+func main() {
+	stderr := bufio.NewWriter(os.Stderr)
+    defer stderr.Flush()
+    
+    stdin := bufio.NewReader(os.Stdin)
+    
+    ids := make([]string, 0, 2000)
+    var line string
+    
+    read, err := fmt.Fscanf(stdin, "%s\n", &line)
+    for ;err == nil && read == 1; {
+        ids = append(ids, line)
+        read, err = fmt.Fscanf(stdin, "%s\n", &line)
+    }
+    fmt.Fprintf(stderr, "Stopped after reading %d lines, with %s, read %d\n", len(ids), err, read)
+
+    for _, id1 := range ids {
+        for _, id2 := range ids {
+            different, common := distance(id1, id2)
+            if different == 1 {
+                fmt.Printf("%s\n", common)
+                os.Exit(0)
+            }
+        }
+    }
+}
